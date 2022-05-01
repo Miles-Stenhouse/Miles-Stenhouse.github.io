05cdef4a29d565e86b08b5e353ae949eeeedfec2
Miles-Stenhouse <miles.stenhouse@sjsu.edu>
2022-05-01 00:56:21 -0500

    bjk

---
+++ p5jsgame/p5jsgame.js
@@ -0,0 +1,32 @@
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+
+function setup() {
+  createCanvas(600, 600);
+  textAlign(CENTER);
+  textSize(20);
+  
+}
+function draw() {
+  background(220);
+  
+  levelOne();
+  
+  text(("Score " + score),width/2,40);
+}
+
+function levelOne(){
+  text("Level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if(distToBall < ballSize/2){
+    ballx = random(width);
+    bally = random(height);
+    score = score + 1;
+  }
+}
+if(score>= 5){
+  background(random(255));
+}
+
+line(ballx, bally, mouseX, mouseY);
