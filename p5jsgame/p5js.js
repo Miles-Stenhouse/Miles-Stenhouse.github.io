e3206c94403f57dedbf78787d459b1e8b6f81aa0
Miles-Stenhouse <miles.stenhouse@sjsu.edu>
2022-05-01 02:18:43 -0500

    m.;

--- p5jsgame/p5js.js
+++ p5jsgame/p5js.js
@@ -1,9 +1,13 @@
 var ballx = 300;
 var bally = 300;
-var ballSize = 40;
+var ballSize = 70;
 var score =0;
 var gameState= "L1";
 
+function preload() {
+ img1 = loadImage('https://miles-stenhouse.github.io/p5jsgame/fly.png');
+    img2 = loadImage('https://miles-stenhouse.github.io/p5jsgame/cow.png');
+}
 
 function setup() {
   createCanvas(600, 600);
@@ -13,7 +17,7 @@ function setup() {
 
 
 function draw() {
-  background(220);
+  background(img2);
   if (gameState=="L1"){
   levelOne();
   } 
@@ -44,13 +48,13 @@ function levelOne(){
  gameState= "L2";
   }
   
-  ellipse(ballx, bally, ballSize, ballSize);
-  line(ballx, bally, mouseX, mouseY);
+  image(img1, ballx, bally, ballSize, ballSize);
+//  line(ballx, bally, mouseX, mouseY);
   
 } // end level one
 
 function levelTwo(){
-  background(200, 100, 0);
+  background(img2);
   text("Level 2", width/2, height-20);
   var distToBall= dist(ballx, bally, mouseX, mouseY);
   if (distToBall <ballSize/2){
@@ -61,15 +65,15 @@ function levelTwo(){
   if(score>10){
 // lvel 3
    gameState = "L3";
-
   }
   
-//  line(ballx, bally, mouseX, mouseY);
-  ellipse(ballx, bally, ballSize, ballSize);
+  image(img1, ballx, bally, ballSize, ballSize);
+ // line(ballx, bally, mouseX, mouseY);
+ 
 } // end level two
 
 function levelThree(){
-    background(200, 100, 2000);
+    background(img2);
   text("Level 3", width/2, height-20);
   var distToBall= dist(ballx, bally, mouseX, mouseY);
   if (distToBall <ballSize/2){
@@ -85,6 +89,6 @@ function levelThree(){
 
   }
   
-//  line(ballx, bally, mouseX, mouseY);
-  ellipse(ballx, bally, ballSize, ballSize);
+// line(ballx, bally, mouseX, mouseY);
+  image( img1, ballx, bally, ballSize, ballSize);
 } // end level thre
